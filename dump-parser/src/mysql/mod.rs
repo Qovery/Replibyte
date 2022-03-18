71ee1b3a930df66130f8c7a1c0510661d94043f8
Reza Handzalah <reza.handzalah@mbiz.co.id>
2022-03-18 14:38:46 +0700

    refactor to test macro

--- dump-parser/src/mysql/mod.rs
+++ dump-parser/src/mysql/mod.rs
@@ -8,24 +8,28 @@ pub struct SQLParser;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    macro_rules! test_tokenize_statement {
+        ( $( $name:ident : $s:literal),* ) => {
+            $(
+                mod $name {
+                    #[test]
+                    fn test_tokenize_statement() {
+                        use crate::mysql::*;
 
-    fn print_tokens(statement: &str) {
-        let parsed = SQLParser::parse(Rule::file, statement)
-            .expect("unsuccessful parse")
-            .next()
-            .expect("pest failure");
+                        let parsed = SQLParser::parse(Rule::file, $s)
+                            .expect("unsuccessful parse")
+                            .next()
+                            .expect("pest failure");
 
-        parsed.tokens().for_each(|x| println!("{:?}", x));
+                        parsed.tokens().for_each(|x| println!("{:?}", x));
+                    }
+                }
+            )*
+        }
     }
 
-    #[test]
-    fn create_table_statement() {
-        print_tokens("CREATE TABLE public.orders ( order_id smallint NOT NULL );");
-    }
-
-    #[test]
-    fn insert_into_statement() {
-        print_tokens("INSERT INTO public.customers (customer_id, company_name, contact_name, contact_title) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders', NULL);");
+    test_tokenize_statement! {
+        create_table: "CREATE TABLE public.orders ( order_id smallint NOT NULL );",
+        insert_into: "INSERT INTO public.customers (customer_id, company_name, contact_name, contact_title) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders', NULL);"
     }
 }
