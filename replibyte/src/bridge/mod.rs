3ddba75a37c4f65b8cdd1e67f9c6e87f9d9f55b2
Romaric Philogene <evoxmusic@gmail.com>
2022-03-19 11:47:39 +0100

    wip: implements index file on s3

--- replibyte/src/bridge/mod.rs
+++ replibyte/src/bridge/mod.rs
@@ -1,25 +1,28 @@
 use crate::connector::Connector;
 use crate::types::{Queries, Query};
+use serde::{Deserialize, Serialize};
 use std::io::Error;
 
 pub mod s3;
 
-pub struct IndexFile {
-    backups: Vec<Backup>,
-}
-
-pub struct Backup {
-    directory_name: String,
-    size: usize,
-    created_at: u64,
-}
-
 pub trait Bridge: Connector + Send + Sync {
     /// Getting Index file with all the backups information
     fn index_file(&self) -> Result<IndexFile, Error>;
-    fn save(&self, index_file: IndexFile) -> Result<(), Error>;
+    fn save(&self, index_file: &IndexFile) -> Result<(), Error>;
     fn upload(&self, file_part: u16, queries: &Queries) -> Result<(), Error>;
     fn download<F>(&self, query_callback: F) -> Result<(), Error>
     where
         F: FnMut(Query);
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct IndexFile {
+    backups: Vec<Backup>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Backup {
+    directory_name: String,
+    size: usize,
+    created_at: u64,
 }
--- replibyte/src/bridge/s3.rs
+++ replibyte/src/bridge/s3.rs
@@ -2,14 +2,18 @@ use std::io::{Error, ErrorKind};
 
 use aws_config::provider_config::ProviderConfig;
 use aws_sdk_s3::model::{BucketLocationConstraint, CreateBucketConfiguration};
+use aws_sdk_s3::types::ByteStream;
 use aws_sdk_s3::Client;
 use aws_types::os_shim_internal::Env;
 use log::{error, info};
 
+use crate::bridge::s3::S3Error::FailedObjectUpload;
 use crate::bridge::{Bridge, IndexFile};
 use crate::connector::Connector;
 use crate::runtime::block_on;
 use crate::types::{Queries, Query};
+
+const INDEX_FILE_NAME: &str = "metadata.json";
 
 pub struct S3 {
     bucket: String,
@@ -82,26 +86,42 @@ impl S3 {
         Ok(())
     }
 
-    fn create_index_file(&self) -> Result<(), Error> {
-        // TODO: Implement index creation logic
-        Ok(())
+    fn create_index_file(&self) -> Result<IndexFile, Error> {
+        match self.index_file() {
+            Ok(index_file) => Ok(index_file),
+            Err(_) => {
+                let index_file = IndexFile { backups: vec![] };
+                let _ = self.save(&index_file)?;
+                Ok(index_file)
+            }
+        }
     }
 }
 
 impl Connector for S3 {
     fn init(&mut self) -> Result<(), Error> {
-        self.create_bucket()
+        let _ = self.create_bucket()?;
+        self.create_index_file().map(|_| ())
     }
 }
 
 impl Bridge for S3 {
     fn index_file(&self) -> Result<IndexFile, Error> {
-        // ? TODO: Implement index file search
-        Ok(IndexFile { backups: vec![] })
+        let object = get_object(&self.client, self.bucket.as_str(), INDEX_FILE_NAME)?;
+        let index_file: IndexFile = serde_json::from_slice(object.as_slice())?;
+        Ok(index_file)
     }
 
-    fn save(&self, index_file: IndexFile) -> Result<(), Error> {
-        Ok(())
+    fn save(&self, index_file: &IndexFile) -> Result<(), Error> {
+        let index_file_json = serde_json::to_vec(index_file)?;
+
+        create_object(
+            &self.client,
+            self.bucket.as_str(),
+            INDEX_FILE_NAME,
+            index_file_json,
+        )
+        .map_err(|err| Error::from(err))
     }
 
     fn upload(&self, file_part: u16, queries: &Queries) -> Result<(), Error> {
@@ -117,10 +137,112 @@ impl Bridge for S3 {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
+enum S3Error<'a> {
+    ObjectDoesNotExist { bucket: &'a str, key: &'a str },
+    FailedObjectDownload { bucket: &'a str, key: &'a str },
+    FailedObjectUpload { bucket: &'a str, key: &'a str },
+    FailedToDeleteObject { bucket: &'a str, key: &'a str },
+}
+
+impl<'a> From<S3Error<'a>> for Error {
+    fn from(err: S3Error<'a>) -> Self {
+        match err {
+            S3Error::ObjectDoesNotExist {
+                bucket,
+                key: object,
+            } => Error::new(
+                ErrorKind::Other,
+                format!("object '{}/{}' does not exist", bucket, object),
+            ),
+            S3Error::FailedObjectDownload {
+                bucket,
+                key: object,
+            } => Error::new(
+                ErrorKind::Other,
+                format!("failed to download object '{}/{}'", bucket, object),
+            ),
+            FailedObjectUpload {
+                bucket,
+                key: object,
+            } => Error::new(
+                ErrorKind::Other,
+                format!("failed to upload object '{}/{}'", bucket, object),
+            ),
+            S3Error::FailedToDeleteObject {
+                bucket,
+                key: object,
+            } => Error::new(
+                ErrorKind::Other,
+                format!("failed to delete object '{}/{}'", bucket, object),
+            ),
+        }
+    }
+}
+
+fn create_object<'a>(
+    client: &Client,
+    bucket: &'a str,
+    key: &'a str,
+    object: Vec<u8>,
+) -> Result<(), S3Error<'a>> {
+    let result = block_on(
+        client
+            .put_object()
+            .bucket(bucket)
+            .key(key)
+            .body(ByteStream::from(object))
+            // TODO: set metadata etag to validate upload on the S3 side
+            .send(),
+    );
+
+    if let Err(_) = result {
+        return Err(S3Error::FailedObjectUpload { bucket, key });
+    }
+
+    Ok(())
+}
+
+fn get_object<'a>(client: &Client, bucket: &'a str, key: &'a str) -> Result<Vec<u8>, S3Error<'a>> {
+    let result = block_on(client.get_object().bucket(bucket).key(key).send());
+
+    match result {
+        Ok(file) => match block_on(file.body.collect()) {
+            Ok(data) => Ok(data.into_bytes().to_vec()),
+            Err(_) => Err(S3Error::FailedObjectDownload { bucket, key }),
+        },
+        Err(_) => Err(S3Error::ObjectDoesNotExist { bucket, key }),
+    }
+}
+
+fn delete_object<'a>(client: &Client, bucket: &'a str, key: &'a str) -> Result<(), S3Error<'a>> {
+    let _ = get_object(client, bucket, key)?;
+
+    let result = block_on(client.delete_object().bucket(bucket).key(key).send());
+
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(S3Error::FailedToDeleteObject { bucket, key }),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::fmt::format;
+
+    use fake::{Fake, Faker};
+
+    use crate::bridge::s3::{create_object, delete_object, get_object, S3Error};
+    use crate::bridge::Bridge;
     use crate::connector::Connector;
     use crate::S3;
+
+    const BUCKET_NAME: &str = "replibyte-test";
+    const REGION: &str = "us-east-2";
+
+    fn bucket() -> String {
+        format!("replibyte-test-{}", Faker.fake::<String>().to_lowercase())
+    }
 
     fn credentials() -> (String, String) {
         (
@@ -129,11 +251,11 @@ mod tests {
         )
     }
 
-    fn s3() -> S3 {
+    fn s3(bucket: &str) -> S3 {
         let (access_key_id, secret_access_key) = credentials();
 
         S3::new(
-            "replibyte-test",
+            bucket,
             "us-east-2",
             access_key_id.as_str(),
             secret_access_key.as_str(),
@@ -142,7 +264,92 @@ mod tests {
 
     #[test]
     fn init_s3() {
-        let mut s3 = s3();
+        let bucket = bucket();
+        let mut s3 = s3(bucket.as_str());
+        // executed twice to check that there is no error at the second call
         assert!(s3.init().is_ok());
+        assert!(s3.init().is_ok());
+    }
+
+    #[test]
+    fn create_and_get_and_delete_object() {
+        let bucket = bucket();
+
+        let mut s3 = s3(bucket.as_str());
+        let _ = s3.init().expect("s3 init failed");
+
+        let key = format!("testing-object-{}", Faker.fake::<String>());
+
+        assert_eq!(
+            get_object(&s3.client, bucket.as_str(), key.as_str())
+                .err()
+                .unwrap(),
+            S3Error::ObjectDoesNotExist {
+                bucket: bucket.as_str(),
+                key: key.as_str(),
+            }
+        );
+
+        assert!(create_object(
+            &s3.client,
+            bucket.as_str(),
+            key.as_str(),
+            b"hello w0rld".to_vec(),
+        )
+        .is_ok());
+
+        assert_eq!(
+            get_object(&s3.client, BUCKET_NAME, key.as_str()).unwrap(),
+            b"hello w0rld"
+        );
+
+        // check that the object is updated
+        assert!(create_object(
+            &s3.client,
+            bucket.as_str(),
+            key.as_str(),
+            b"hello w0rld updated".to_vec(),
+        )
+        .is_ok());
+
+        assert_eq!(
+            get_object(&s3.client, bucket.as_str(), key.as_str()).unwrap(),
+            b"hello w0rld updated"
+        );
+
+        assert!(delete_object(&s3.client, bucket.as_str(), key.as_str()).is_ok());
+
+        assert_eq!(
+            delete_object(&s3.client, bucket.as_str(), key.as_str())
+                .err()
+                .unwrap(),
+            S3Error::ObjectDoesNotExist {
+                bucket: bucket.as_str(),
+                key: key.as_str(),
+            }
+        );
+
+        assert_eq!(
+            get_object(&s3.client, bucket.as_str(), key.as_str())
+                .err()
+                .unwrap(),
+            S3Error::ObjectDoesNotExist {
+                bucket: bucket.as_str(),
+                key: key.as_str(),
+            }
+        );
+    }
+
+    #[test]
+    fn test_s3_index_file() {
+        let bucket = bucket();
+        let mut s3 = s3(bucket.as_str());
+        let _ = s3.init().expect("s3 init failed");
+
+        assert!(s3.index_file().is_ok());
+
+        let index_file = s3.index_file().unwrap();
+
+        assert!(index_file.backups.is_empty());
     }
 }
