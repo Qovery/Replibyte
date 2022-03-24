a38ace6a9390696dd7eb4d8d030c137fd8a8605d
benny-n <benick12@gmail.com>
2022-03-24 02:28:40 +0200

    wip: full backup task with mongo source

--- replibyte/src/main.rs
+++ replibyte/src/main.rs
@@ -22,6 +22,8 @@ use crate::config::{Config, ConnectionUri};
 use crate::connector::Connector;
 use crate::destination::postgres::Postgres as DestinationPostgres;
 use crate::destination::postgres_stdout::PostgresStdout;
+use crate::source::mongo::Mongo as SourceMongo;
+use crate::source::mongo_stdin::MongoStdin;
 use crate::source::postgres::Postgres as SourcePostgres;
 use crate::source::postgres_stdin::PostgresStdin;
 use crate::source::Source;
@@ -177,7 +179,16 @@ fn main() -> anyhow::Result<()> {
                                 todo!() // FIXME
                             }
                             ConnectionUri::Mongo(host, port, username, password, database) => {
-                                todo!() // FIXME
+                                let mongo = SourceMongo::new(
+                                    host.as_str(),
+                                    port,
+                                    database.as_str(),
+                                    username.as_str(),
+                                    password.as_str(),
+                                );
+
+                                let task = FullBackupTask::new(mongo, &transformers, bridge);
+                                task.run(progress_callback)?
                             }
                         },
                         // some user use "postgres" and "postgresql" both are valid
